8399b955c17894026d98564de7dd14a64cc75ebc
gabrielgoncalves06 <GABRIELGONCALVESPA0303@GMAIL.COM>
2025-11-22 13:22:02 -0300

    Refactor salvar_personagem_json function

--- Python_oldDragon/model/gerenciador_save.py
+++ Python_oldDragon/model/gerenciador_save.py
@@ -1,4 +1,4 @@
-# models/gerenciador_save.py
+
 import json
 import os
 from model.classes_personagem.personagem import Personagem 
@@ -6,14 +6,9 @@ from model.classes_personagem.personagem import Personagem
 PASTA_SAVES = "saves"
 
 def salvar_personagem_json(personagem: Personagem):
-    """
-    Recebe uma instância de Personagem, a converte para um dicionário
-    e a salva em um arquivo .json.
-    """
-    # Cria a pasta 'saves' se ela não existir
+    
     os.makedirs(PASTA_SAVES, exist_ok=True)
 
-   
     personagem_dict = personagem.to_dict()   # Converte o objeto
 
     # Cria um nome de arquivo seguro (ex: "Aragorn" -> "saves/aragorn.json")
@@ -24,4 +19,4 @@ def salvar_personagem_json(personagem: Personagem):
 
     # Abre o arquivo e salva o JSON
     with open(caminho_arquivo, 'w', encoding='utf-8') as f:
-        json.dump(personagem_dict, f, ensure_ascii=False, indent=4)      # indent=4 deixa o arquivo JSON bonito e legível
+        json.dump(personagem_dict, f, ensure_ascii=False, indent=4)      
